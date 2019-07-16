750c8f98c8e4fad72d027c34988016685a888e04
Tomas Kislan <kislan.tomas@gmail.com>
2019-07-16 09:23:03 +0200

    Minor fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ HOST=${HOST:-localhost}
 HOST_PORT=${HOST_PORT:-8080}
 
 if [ ! -f /root/.ssh/id_rsa ]; then
-  ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
+  /usr/bin/ssh-keygen -b 2048 -t rsa -f /root/.ssh/id_rsa -q -N ""
 fi
 
 cat /root/.ssh/id_rsa.pub
@@ -19,4 +19,4 @@ cat /root/.ssh/id_rsa.pub
 rm -vf /root/.ssh/known_hosts
 
 # Start SSH connection
-exec /usr/bin/ssh -N -p "${SERVER_PORT}" -R "${REMOTE_PORT}":"${HOST}":"${HOST_PORT}" tunnel@"${SERVER_HOST}"
+exec /usr/bin/ssh -N -o "ExitOnForwardFailure yes" -o "ServerAliveInterval=60" -p "${SERVER_PORT}" -R "${REMOTE_PORT}":"${HOST}":"${HOST_PORT}" tunnel@"${SERVER_HOST}"
